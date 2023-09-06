a8ae9e27203e280e5fc500c1c28593cb3d593b49
Asif <asif.bhuiyan04@outlook.com>
2023-09-07 01:20:15 +1000

    Implemented multiple pendulums swinging at once + slider

--- pendulum.py
+++ pendulum.py
@@ -10,14 +10,15 @@ pygame.init()
 ###################################################################
 # Initial variables
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 1000
 BG_COLOUR = (0, 0, 0)
 PENDULUM_COLOUR = (255, 255, 255)
 TRAIL_COLOUR = (57, 255, 20)
 PENDULUM_LENGTH = 200
-INITIAL_ANGLE = math.pi / random.randrange(1, 9)
+INITIAL_ANGLE = math.pi / random.randrange(2, 9)
 GRAVITY = 9.8
 TIME_STEP = 0.1
+NUM_PENDULUMS = 1
 ###################################################################
 # Initialises the GUI for the screen  
 
@@ -35,19 +36,25 @@ root = tk.Tk()
 root.title("Pendulum Controls")
 
 # Create slider variables
+num_pend_var = tk.IntVar(value = NUM_PENDULUMS)
 length_var = tk.DoubleVar(value = PENDULUM_LENGTH)
 angle_var = tk.DoubleVar(value = math.degrees(INITIAL_ANGLE))
 gravity_var = tk.DoubleVar(value = GRAVITY)
 
 # Function to update the simulation parameters
 def update_parameters(*args):
-    global PENDULUM_LENGTH, INITIAL_ANGLE, GRAVITY
+    global PENDULUM_LENGTH, INITIAL_ANGLE, GRAVITY, NUM_PENDULUMS
     PENDULUM_LENGTH = length_var.get()
     INITIAL_ANGLE = math.radians(angle_var.get())
     GRAVITY = gravity_var.get()
+    NUM_PENDULUMS = num_pend_var.get()
     length_label.config(text=f"Pendulum Length (pixels): {int(PENDULUM_LENGTH)}")
     angle_label.config(text=f"Initial Angle (degrees): {int(angle_var.get())}")
     gravity_label.config(text=f"Gravity (m/s^2): {gravity_var.get():.2f}")
+    num_pend_label.config(text=f"Number of Pendulums: {int(NUM_PENDULUMS)}")
+    
+num_pend_label = ttk.Label(root, text=f"Number of Pendulums: {int(NUM_PENDULUMS)}")
+num_pend_slider = ttk.Scale(root, variable=num_pend_var, from_=1, to=8, orient="horizontal")
 
 length_label = ttk.Label(root, text=f"Pendulum Length (pixels): {int(PENDULUM_LENGTH)}")
 length_slider = ttk.Scale(root, variable=length_var, from_=50, to=300, orient="horizontal")
@@ -59,6 +66,8 @@ gravity_label = ttk.Label(root, text=f"Gravity (m/s^2): {gravity_var.get():.2f}"
 gravity_slider = ttk.Scale(root, variable=gravity_var, from_=1, to=20, orient="horizontal")
 
 # Pack widgets into the Tkinter window
+num_pend_label.pack()
+num_pend_slider.pack()
 length_label.pack()
 length_slider.pack()
 angle_label.pack()
@@ -66,10 +75,14 @@ angle_slider.pack()
 gravity_label.pack()
 gravity_slider.pack()
 
+# Updates the data on the sliders 
+num_pend_var.trace("w", update_parameters)
 length_var.trace("w", update_parameters)
 angle_var.trace("w", update_parameters)
 gravity_var.trace("w", update_parameters)
 
+###################################################################
+# Main loop function for the pendulum simulation
 def run_simulation():
     global running, TIME_STEP
     while running:
@@ -78,21 +91,25 @@ def run_simulation():
                 running = False
                 
         screen.fill(BG_COLOUR)
-        theta = INITIAL_ANGLE * math.cos(math.sqrt(GRAVITY / PENDULUM_LENGTH) * TIME_STEP)  
         
-        pend_x = SCREEN_WIDTH // 2 + PENDULUM_LENGTH * math.sin(theta)
-        pend_y = SCREEN_HEIGHT // 2 + PENDULUM_LENGTH * math.cos(theta)
-    
-        trail_pos.append((pend_x, pend_y))
-        if len(trail_pos) > 100:
-            trail_pos.pop(0)
+        for i in range(0, NUM_PENDULUMS):
+            pend_len = PENDULUM_LENGTH + (20 * i)
+            theta = INITIAL_ANGLE * math.cos(math.sqrt(GRAVITY / pend_len) * TIME_STEP)  
+            
+            pend_x = SCREEN_WIDTH // 2 + pend_len * math.sin(theta)
+            pend_y = SCREEN_HEIGHT // 2 + pend_len * math.cos(theta)
         
-        for i in range(1, len(trail_pos)):
-            pygame.draw.aaline(screen, TRAIL_COLOUR, trail_pos[i - 1], trail_pos[i])  
-        
-        pygame.draw.line(screen, PENDULUM_COLOUR, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), (pend_x, pend_y), 1)
-        pygame.draw.circle(screen, PENDULUM_COLOUR, (pend_x, pend_y), 10)
-        
+            if NUM_PENDULUMS == 1:
+                trail_pos.append((pend_x, pend_y))
+                if len(trail_pos) > 100:
+                    trail_pos.pop(0)
+                
+                for j in range(1, len(trail_pos)):
+                    pygame.draw.aaline(screen, TRAIL_COLOUR, trail_pos[j - 1], trail_pos[j])  
+            
+            pygame.draw.line(screen, PENDULUM_COLOUR, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), (pend_x, pend_y), 1)
+            pygame.draw.circle(screen, (255 - (i * 20), 255, 255 - (i * 20)), (pend_x, pend_y), 10)
+            
         pygame.display.flip()
         clock.tick(60)
         TIME_STEP += 0.1
